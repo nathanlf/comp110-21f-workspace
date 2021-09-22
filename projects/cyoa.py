67db379b680294d4667d6e6fa74d72f15fbb5073
Nathan Flinchum <nflinchum@unc.edu>
2021-09-22 01:31:37 -0400

    PJ00 Almost done?

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -2,6 +2,8 @@
 
 __author__ = "730483098"
 
+import random  # import at top of file
+
 # Emoji Constants
 WELCOME: str = "\U0000203C"
 FIVE_GUESSES_LEFT: str = "\U0001f600"
@@ -10,14 +12,15 @@ THREE_GUESSES_LEFT: str = "\U0001f610"
 TWO_GUESSES_LEFT: str = "\U0001f626"
 ONE_GUESS_LEFT: str = "\U0001f630"
 GAME_OVER: str = "\U0001F614"
+HEART: str = "\U0001F496"
 
 # Upper and Lower bounds for range of numbers
 UPPER: int = 5
 LOWER: int = 1
 
-points: int  # make number guessing game - Make it so every time they guess correctly, they get an extra "LIFE" (guess)
-player: str  # make it so the player has multiple lives, points for how many times they get it right - use emojis for fun
-guesses_remaining: int = 5
+# Global Variables
+points: int 
+player: str
 
 
 def main() -> None:
@@ -25,46 +28,76 @@ def main() -> None:
     global points  # Ask about this? I don't think this is fine - This doesn't make sense to me - office hours! zoom
     global guesses_remaining
     points = 0
+    guesses_remaining: int = 5  # Player starts with 5 "lives"
+    user_in: int = 0
+
     greet()
 
-    # Program starts now! 1. Make a random number between 1 and 5
-    # Compare function ? User guess and randnum as parameters? 
-    
-    # Game loop - Would you like to play again? Keep track of total points accumulated / highest streak? - add a quit option obviously
-    import random
-    rand_num: int
+    while user_in != 2:  # Game Loop
+        points = 0  # Points reset after each game session
+        guesses_remaining = 5  # Player "lives" reset after each game session
+        while guesses_remaining > 0:
+            rand_num: int = random.randint(LOWER, UPPER)
+            guess: int = int(input((f"\nGuess the number between {LOWER} and {UPPER}: ")))
+            result: int = compare(guess, rand_num)
+            
+            if result == 1:
+                print("Correct!")
+                points += 1
+                guesses_remaining += 1
+            else:
+                print("Incorrect.")
+                encourage()  # Gives player encouragement if they guess wrong
+                guesses_remaining -= 1
 
-    while guesses_remaining > 0:
-        rand_num = random.randint(LOWER, UPPER)
-        guess: int = int(input((f"Guess the number between {LOWER} and {UPPER}: ")))
+            if guesses_remaining > 0:
+                print(f"Current Points: {points}\nCurrent Guesses Remaining: {guesses_remaining}\n")
         
-        result: bool = compare(guess, rand_num)
-        
-        if result and guesses_remaining > 0:
-            points += 1
-            guesses_remaining += 1
-        else:
-            guesses_remaining -= 1
+        print(f"Your final score is: {points}")
+        user_in = int(input("(1) Continue Playing\n(2) Exit the game\n"))
+
+        if user_in == 2:
+            guesses_remaining = 0  # To exit the game
+
+    print(f"\nThanks for playing {player}! Goodbye.")
 
 
 def greet() -> None:
     """Greets the user and provides information about the game."""
-    print(f"Welcome {WELCOME + FIVE_GUESSES_LEFT} The objective of this game is to guess the correct number in a certain range of integers.")
+    print(f"Welcome{WELCOME + FIVE_GUESSES_LEFT} The objective of this game is to guess the correct number in a certain range of integers.")
     print("Each time you guess the number correctly, you gain one point!\n")
-    print("The number you're trying to guess will remain the same until you either guess the number or run out of guesses.")
-    print("The game will keep track of your total numbers guessed correctly and how many guesses you have left.\n")
+    print("The number you're trying to guess will be a new random number (that could potentially be the same) each time you guess.")
+    print("The game will keep track of your total numbers guessed correctly (points) and how many guesses you have left.\n")
+    print("P.S. Each time you guess correctly, you gain an extra guess! Also, encouraging messages will be provided.")
 
     player = input("Please enter your name: ")
 
     print(f"Good luck, {player}, and have fun!\n")
 
 
-def compare(guess: int, target: int) -> bool:
+def compare(guess: int, target: int) -> int:
     """Compares two integer values for equality."""
     if guess == target:
-        return True
+        return 1
     else:
-        return False
+        return 2
+
+
+def encourage() -> None:
+    """Provides encouragement to the user."""
+    global player
+    num = random.randint(1, 4)
+
+    if num == 1:
+        print(f"You're doing better than you know! Keep pushing, {player}!")
+    elif num == 2:
+        print(f"Has anyone ever told you that you're incredible, {player}? Because I am right now.")
+    elif num == 3:
+        print(f"I know this game may be a little tough at times, but I believe in you, {player}.")
+    elif num == 4:
+        print(f"YOU{HEART}GOT{HEART}THIS{HEART}{player}{HEART}")
+    else:
+        print(f"{HEART+HEART+HEART+HEART+HEART+HEART+HEART+HEART+HEART+HEART}i love u {player} keep going")
 
 
 if __name__ == "__main__":
