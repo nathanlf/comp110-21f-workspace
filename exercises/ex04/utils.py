d620172f18fcdd20a4416299ffd6d20c3eca2ac7
Nathan Flinchum <nflinchum@unc.edu>
2021-09-26 22:42:05 -0400

    Ex04 1/3

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,16 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730483098"
 
 
-# TODO: Implement your functions here.
+def all(num_list: list[int], num: int) -> bool:
+    """Returns True if all values in a given list are the same as a given integer. False otherwise."""
+    i: int = 0
+    all_equal: bool = True
+
+    while i < len(num_list):
+        if num_list[i] != num:
+            all_equal = False
+        i += 1
+    
+    return all_equal
