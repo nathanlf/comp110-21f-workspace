1495da820b9ce291c83dc6861fc18c13af52e90c
Nathan Flinchum <nflinchum@unc.edu>
2021-09-28 20:31:49 -0400

    EX04 Final (I think)

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -6,11 +6,44 @@ __author__ = "730483098"
 def all(num_list: list[int], num: int) -> bool:
     """Returns True if all values in a given list are the same as a given integer. False otherwise."""
     i: int = 0
-    all_equal: bool = True
+
+    if len(num_list) == 0:
+        return False
 
     while i < len(num_list):
         if num_list[i] != num:
-            all_equal = False
+            return False
         i += 1
     
-    return all_equal
+    return True
+
+
+def is_equal(nums_one: list[int], nums_two: list[int]) -> bool:
+    """Returns True if lists are equal, False otherwise."""
+    i: int = 0
+
+    if len(nums_one) != len(nums_two):
+        return False
+
+    while i < len(nums_one):
+        if nums_one[i] != nums_two[i]:
+            return False
+        i += 1
+
+    return True
+
+
+def max(in_list: list[int]) -> int:
+    if len(in_list) == 0:
+        raise ValueError("max() arg is an empty List")
+
+    max_num: int = in_list[0]
+    i: int = 0
+
+    while i < len(in_list):
+        if in_list[i] > max_num:
+            max_num = in_list[i]
+
+        i += 1
+    
+    return max_num
