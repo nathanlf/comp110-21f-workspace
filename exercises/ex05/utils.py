85bc32af9ae825f218e4104b003a06c3daf56f3e
Nathan Flinchum <nflinchum@unc.edu>
2021-10-04 14:32:16 -0400

    EX05 Final

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,56 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730483098"
 
 # Define your functions below
+
+
+def only_evens(nums: list[int]) -> list[int]:
+    """Returns a list of even integers."""
+    even_list: list[int] = list()
+    i: int = 0
+    while i < len(nums):
+        if nums[i] % 2 == 0:
+            even_list.append(nums[i])
+        i += 1
+
+    return even_list
+
+
+def sub(nums: list[int], start: int, end: int) -> list[int]:
+    """Returns a subset of a given list of integers."""
+    sub_list: list[int] = list()
+    if len(nums) < 0 or len(nums) < start or end <= 0:
+        return sub_list  # Returns an empty list
+
+    # Additional start/stop variables created to retain initial value of start & end in memory
+    i: int = start
+    stop: int = end
+
+    if start < 0:
+        i = 0
+    if end > len(nums):
+        stop = len(nums)  # When it says "end with the end of the list" - does it mean to include the end INDEX or not?
+    
+    while i < stop:
+        sub_list.append(nums[i])
+        i += 1
+
+    return sub_list
+
+
+def concat(nums_one: list[int], nums_two: list[int]) -> list[int]:
+    """Returns a list of all elements of the first list followed by all elements of the second list."""
+    concat_list: list[int] = list()
+
+    i: int = 0
+    while i < len(nums_one):
+        concat_list.append(nums_one[i])
+        i += 1
+
+    j: int = 0
+    while j < len(nums_two):
+        concat_list.append(nums_two[j])
+        j += 1
+
+    return concat_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,94 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730483098"
+
+
+def test_only_evens_empty() -> None:
+    """Tests when the given list is empty."""
+    nums: list[int] = list()
+    assert only_evens(nums) == []
+
+
+def test_only_evens_single_item_odd() -> None:
+    """Tests when a given list contains a single item that is odd."""
+    nums: list[int] = [1]
+    assert len(only_evens(nums)) == 0
+
+
+def test_only_evens_single_item_even() -> None:
+    """Tests when a given list contains a single item that is even."""
+    nums: list[int] = [2]
+    assert only_evens(nums) == [2]
+
+
+def test_only_evens_many_items_all_odd() -> None:
+    """Tests when a given list contains many items that are all odd."""
+    nums: list[int] = [1, 3, 5]
+    assert only_evens(nums) == []
+
+
+def test_only_evens_many_items_all_even() -> None:
+    """Tests when a given list contains many items that are all even."""
+    nums: list[int] = [2, 4, 6]
+    assert only_evens(nums) == [2, 4, 6]
+
+
+def test_only_evens_many_items_both() -> None:
+    """Tests when a given list contains many items that are both even and odd."""
+    nums: list[int] = [2, 3, 4, 5]
+    assert only_evens(nums) == [2, 4]
+
+
+def test_sub_empty() -> None:
+    """Tests when the given list is empty."""
+    nums: list[int] = list()
+    assert sub(nums, 0, 1) == []
+
+
+def test_sub_single_item() -> None:
+    """Tests when the given list contains one item."""
+    nums: list[int] = [3]
+    assert sub(nums, 0, 1) == [3]
+
+
+def test_sub_many_items() -> None:
+    """Tests when the given list contains many items."""
+    nums: list[int] = [3, 5, 2, -10, 4]
+    assert sub(nums, 1, 3) == [5, 2]
+
+
+def test_concat_both_empty() -> None:
+    """Tests when the given lists are both empty."""
+    nums1: list[int] = list()
+    nums2: list[int] = list()
+    assert concat(nums1, nums2) == []
+
+
+def test_concat_list_one_empty() -> None:
+    """Tests when the first given list is empty and the second contains items."""
+    nums1: list[int] = list()
+    nums2: list[int] = [1, 2, 3]
+    assert concat(nums1, nums2) == [1, 2, 3]
+
+
+def test_concat_list_two_empty() -> None:
+    """Tests when the first given list contains items and the second is empty."""
+    nums1: list[int] = [1, 2, 3]
+    nums2: list[int] = list()
+    assert concat(nums1, nums2) == [1, 2, 3]
+
+
+def test_concat_single_item_both() -> None:
+    """Tests when both given lists contain one item each."""
+    nums1: list[int] = [1]
+    nums2: list[int] = [2]
+    assert concat(nums1, nums2) == [1, 2]
+
+
+def test_concat_many_items_both() -> None:
+    """Tests when both given lists contain many items."""
+    nums1: list[int] = [1, 2, 3]
+    nums2: list[int] = [4, 5, 6]
+    assert concat(nums1, nums2) == [1, 2, 3, 4, 5, 6]
