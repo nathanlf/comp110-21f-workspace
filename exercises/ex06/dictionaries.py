f4e4ab9958336fe586d60d9a92820157d41d6a32
Nathan Flinchum <nflinchum@unc.edu>
2021-10-19 23:42:00 -0400

    Ex06 Final (I think)

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,50 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730483098"
 
-# Define your functions below
+
+def invert(dictionary: dict[str, str]) -> dict[str, str]:
+    """Inverts a dictionary's keys with its values."""
+    inverted: dict[str, str] = dict()
+
+    for key in dictionary:
+        if dictionary[key] in inverted:
+            raise KeyError("There cannot be duplicates of the same key (in the inverse dictionary)!")
+        inverted[dictionary[key]] = key
+
+    return inverted
+
+
+def favorite_color(dictionary: dict[str, str]) -> str:
+    """Returns the most popular color in a list of favorite colors."""
+    colors: dict[str, int] = dict()
+
+    for key in dictionary:
+        color: str = dictionary[key]
+        if color in colors:
+            colors[color] += 1
+        else:
+            colors[color] = 1
+
+    popular: str = ""
+    largest = 0
+    for key in colors:
+        if colors[key] > largest:
+            largest = colors[key]
+            popular = key
+
+    return popular
+
+
+def count(vals: list[str]) -> dict[str, int]:
+    """Returns a dictionary with the values of the list and how frequently they occur."""
+    freq: dict[str, int] = dict()
+    i: int = 0
+    while i < len(vals):
+        if vals[i] in freq:
+            freq[vals[i]] += 1
+        else:
+            freq[vals[i]] = 1
+        i += 1
+
+    return freq
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730483098"
+
+
+def test_invert_empty() -> None:
+    """Tests when the given dictionary is empty."""
+    dictionary: dict[str, str] = dict()
+    assert invert(dictionary) == {}
+
+
+def test_invert_one_pair() -> None:
+    """Tests when the given dictionary has one key-value pair."""
+    dictionary: dict[str, str] = {'a': 'b'}
+    assert invert(dictionary) == {'b': 'a'}
+
+
+def test_invert_multiple_pairs() -> None:
+    """Tests when the given dictionary has one key-value pair."""
+    dictionary: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
+    assert invert(dictionary) == {'b': 'a', 'd': 'c', 'f': 'e'}
+
+
+def test_favorite_color_empty() -> None:
+    """Tests when the given dictionary is empty."""
+    dictionary: dict[str, str] = {}
+    assert favorite_color(dictionary) == ""
+
+
+def test_favorite_color_one_pair() -> None:
+    """Tests when the given dictionary has one key-value pair."""
+    dictionary: dict[str, str] = {'Nathan': 'blue'}
+    assert favorite_color(dictionary) == "blue"
+
+
+def test_favorite_color_multiple_pairs() -> None:
+    """Tests when the given dictionary has more than one key-value pair."""
+    dictionary: dict[str, str] = {'Nathan': 'blue', 'Josh': 'red', 'Khat': 'red'}
+    assert favorite_color(dictionary) == "red"
+
+
+def test_count_empty() -> None:
+    """Tests when the given list is empty."""
+    my_list: list[str] = list()
+    assert count(my_list) == {}
+
+
+def test_count_one_value() -> None:
+    """Tests when the given list has one value."""
+    my_list: list[str] = ['uno']
+    assert count(my_list) == {'uno': 1}
+
+
+def test_count_multiple_values() -> None:
+    """Tests when the given list has more than one value."""
+    my_list: list[str] = ['uno', 'dos', 'dos']
+    assert count(my_list) == {'uno': 1, 'dos': 2}
